7ece905524741cf1d20f8e2ada299c9d56bea32e
Shis <sizonets.nikita1@gmail.com>
2024-12-31 09:03:16 +0200

    Add initial version of the event manager with functionality for adding and retrieving events with docker

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using EventManagementLibrary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Створюємо об'єкти Event
+        Event event1 = new Event("Conference", new DateTime(2024, 12, 25, 10, 0, 0), "Kyiv");
+        Event event2 = new Event("Workshop", new DateTime(2024, 12, 26, 14, 0, 0), "Lviv");
+
+        // Створюємо менеджер подій
+        EventManager eventManager = new EventManager();
+
+        // Додаємо заходи
+        eventManager.AddEvent(event1);
+        eventManager.AddEvent(event2);
+
+        // Отримуємо список заходів через GetAllEvents
+        foreach (Event @event in eventManager.GetAllEvents())
+        {
+            Console.WriteLine($"Name = {@event.Name}, Date = {@event.Date}, Location = {@event.Location}");
+        }
+    }
+}
