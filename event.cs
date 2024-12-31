ed6768bbedfce33be04bb6ffc26ef65de71e98e9
Shis <sizonets.nikita1@gmail.com>
2024-12-31 05:38:41 +0200

    add new class "event"

---
+++ event.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace EventManagementLibrary
+{
+    public class Event
+    {
+        public string Name { get; }
+        public DateTime Date { get; }
+        public string Location { get; }
+
+        public Event(string name, DateTime date, string location)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+                throw new ArgumentException("Назва не може бути порожньою.", nameof(name));
+
+            if (string.IsNullOrWhiteSpace(location))
+                throw new ArgumentException("Локація не може бути порожньою.", nameof(location));
+
+            Name = name;
+            Date = date;
+            Location = location;
+        }
+    }
+}
+
+
