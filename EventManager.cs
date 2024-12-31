4d8f7c2015cd7e2f1ba2553f779d769bb2510a70
Shis <sizonets.nikita1@gmail.com>
2024-12-31 05:42:50 +0200

    Added EventManager class for event management

---
+++ EventManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace EventManagementLibrary
+{
+    public class EventManager
+    {
+        private readonly List<Event> _events = new List<Event>();
+
+        public void AddEvent(Event eventInstance)
+        {
+            if (_events.Any(e => e.Name == eventInstance.Name && e.Date == eventInstance.Date))
+                throw new InvalidOperationException("Захід із такою ж назвою і датою вже існує.");
+            _events.Add(eventInstance);
+        }
+
+        public void RemoveEvent(string eventName)
+        {
+            var eventToRemove = _events.FirstOrDefault(e => e.Name == eventName);
+            if (eventToRemove == null)
+                throw new KeyNotFoundException("Захід із заданою назвою не знайдено.");
+            _events.Remove(eventToRemove);
+        }
+
+        public IReadOnlyList<Event> GetAllEvents()
+        {
+            return _events.AsReadOnly();
+        }
+    }
+}
+
