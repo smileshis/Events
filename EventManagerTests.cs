fc61104215d87b6fc2f3f0af7ad6af9cfec26ca1
Shis <sizonets.nikita1@gmail.com>
2024-12-31 05:56:22 +0200

    added new test AddEvent_ShouldAddEventToLibrary

---
+++ EventManagerTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xunit;
+using EventManagementLibrary;
+
+namespace EventManagementTests
+{
+    public class EventManagerTests
+    {
+        private readonly EventManager _eventManager;
+
+        public EventManagerTests()
+        {
+            _eventManager = new EventManager();
+        }
+
+        [Fact]
+        public void AddEvent_ShouldAddEventToLibrary()
+        {
+            // Arrange
+            var eventInstance = new Event("Conference", new DateTime(2024, 12, 25, 10, 0, 0), "Kyiv");
+
+            // Act
+            _eventManager.AddEvent(eventInstance);
+
+            // Assert
+            var events = _eventManager.GetAllEvents();
+            Assert.Contains(eventInstance, events);
+        }
+    }
+}
